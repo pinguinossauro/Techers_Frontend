b83986937d801d3616baed29b14552b857a5f835
Henrique Davi Nery Rueda <henrique.rueda>
2026-03-19 15:58:55 -0300

    Feat: filmes

---
+++ aula 6/script.js
@@ -0,0 +1,23 @@
+//console.log("Hello world")
+
+//3*5
+
+//console.log("3*5")
+//console.log(3*5)
+
+//.const
+//.var
+//.let
+
+
+//for(let i = 0; i <= 10; i++){
+//    console.log(i)
+//}
+
+//for(let i = 10; i <= 0; i++){
+//    console.log(i)
+//}
+
+
+//for (let i = 0; i < frutas.length; i++){
+//    console.log(frutas[i])  
